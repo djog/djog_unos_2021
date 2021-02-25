25ebdcd495ffe5d8eb872f028d183a655075c7ae
rijkvp <rijkvanputten@hotmail.nl>
2021-02-25 18:51:08 +0100

    Improved combat

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public float fireDelay = 0.1f;
+    [Header("Bullet")]
     public GameObject BulletPrefab;
     public float spawnOffset = 1.0f;
     public float spawnZ = 10.0f;
-    
+
+    private float fireTimer;
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (fireTimer > 0.0f)
         {
+            fireTimer -= Time.deltaTime;
+        }
+        if (Input.GetMouseButton(0) && fireTimer <= 0.0)
+        {
+            fireTimer = fireDelay;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 ownPos = new Vector2(transform.position.x, transform.position.y);
             Vector2 direction = (mousePos - ownPos).normalized;
