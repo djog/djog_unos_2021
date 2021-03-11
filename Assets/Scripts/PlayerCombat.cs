835731249fbafd4cdb7c238784c49aeab1c81c2a
Anton Hensen <antonhensen@gmail.com>
2021-03-11 22:00:05 +0100

    - Ability to pickup weapons - Added door

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -15,17 +15,37 @@ public class PlayerCombat : MonoBehaviour
         Vector2 direction = (mousePos - ownPos).normalized;
         Vector3 spawnPos = ownPos + direction * spawnOffset;
         float degrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        weapon.transform.rotation = Quaternion.Euler(0.0f, 0.0f, degrees);
 
-        if (fireTimer > 0.0f)
+        if (weapon != null)
         {
+          weapon.transform.rotation = Quaternion.Euler(0.0f, 0.0f, degrees);
+
+          if (fireTimer > 0.0f)
+          {
             fireTimer -= Time.deltaTime;
-        }
-        if (Input.GetMouseButton(0) && fireTimer <= 0.0)
-        {
+          }
+
+          if (Input.GetMouseButton(0) && fireTimer <= 0.0)
+          {
             // Spawn a bullet
             fireTimer = weapon.fireDelay;
             var bullet = Instantiate(weapon.bulletPrefab, weapon.bulletSpawn.position, weapon.bulletSpawn.rotation);
+          }
         }
     }
-}
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      if (collision.gameObject.tag == "Weapon")
+      {
+        if (weapon != null)
+        {
+          Destroy(weapon.gameObject);
+        }
+        weapon = collision.gameObject.GetComponent<Weapon>();
+        weapon.gameObject.transform.SetParent(gameObject.transform);
+        weapon.transform.localPosition = new Vector3(0,0,0);
+      }
+    }
+  }
