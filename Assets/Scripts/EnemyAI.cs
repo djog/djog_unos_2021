37b5d1db061bf367b52d3bb6069ec5fa5b9613d7
Anton Hensen <antonhensen@gmail.com>
2021-03-25 20:57:37 +0100

    fic null pointer when player is dead and non existent

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,127 +7,131 @@ using Pathfinding;
 [RequireComponent(typeof(Rigidbody2D))]
 public class EnemyAI : MonoBehaviour
 {
-    public float moveSpeed = 6.0f;
-    public float nextWaypointDist = 1.2f;
+  public float moveSpeed = 6.0f;
+  public float nextWaypointDist = 1.2f;
 
-    [Header("Combat")]
-    public Weapon weapon;
-    public GameObject BulletPrefab;
-    public float spawnOffset = 1.0f;
-    public float spawnZ = 10.0f;
+  [Header("Combat")]
+  public Weapon weapon;
+  public GameObject BulletPrefab;
+  public float spawnOffset = 1.0f;
+  public float spawnZ = 10.0f;
 
-    private float fireTimer;
+  private float fireTimer;
 
 
-    private Rigidbody2D rb;
-    private Transform target;
-    private Seeker seeker;
-    private Path path;
-    private bool reachedDestination;
-    private int currentWaypoint;
+  private Rigidbody2D rb;
+  private Transform target;
+  private Seeker seeker;
+  private Path path;
+  private bool reachedDestination;
+  private int currentWaypoint;
 
-    public void Start()
+  public void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    seeker = GetComponent<Seeker>();
+    target = GameManager.GetTarget();
+
+    // Reapeat on seperate thread to safe performance
+    InvokeRepeating("UpdatePath", 0, 0.2f);
+  }
+
+  void UpdatePath()
+  {
+    if (seeker.IsDone() && target != null)
     {
-        rb = GetComponent<Rigidbody2D>();
-        seeker = GetComponent<Seeker>();
-        target = GameManager.GetTarget();
+      seeker.StartPath(transform.position, target.position, FoundPath);
+    }
+  }
 
-        // Reapeat on seperate thread to safe performance
-        InvokeRepeating("UpdatePath", 0, 0.2f);
+  public void FoundPath(Path p)
+  {
+    if (!target)
+    {
+      return;
+    }
+    if (!p.error)
+    {
+      path = p;
+      currentWaypoint = 0;
+    }
+  }
+
+  void Update()
+  {
+
+    if (target != null)
+    {
+      Vector2 direction = (target.position - transform.position).normalized;
+      float degrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+      weapon.transform.rotation = Quaternion.Euler(0.0f, 0.0f, degrees);
+      // Combat
+      if (fireTimer > 0.0f)
+      {
+        fireTimer -= Time.deltaTime;
+      }
+      if (fireTimer <= 0.0)
+      {
+        Fire(direction);
+      }
+    }
+  }
+
+  void Fire(Vector2 direction)
+  {
+    if (!target)
+    {
+      return;
     }
 
-    void UpdatePath()
-    {
-        if (seeker.IsDone() && target != null)
-        {
-            seeker.StartPath(transform.position, target.position, FoundPath);
-        }
-    }
+    Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+    Vector3 spawnPos = currentPosition + direction * spawnOffset;
 
-    public void FoundPath(Path p)
+    RaycastHit2D hitInfo = Physics2D.Raycast(spawnPos, direction);
+    if (hitInfo.collider != null)
     {
-        if (!target) {
-            return;
-        }
-        if (!p.error)
-        {
-            path = p;
-            currentWaypoint = 0;
-        }
-    }
+      Debug.DrawLine(spawnPos, hitInfo.point);
+      if (hitInfo.collider.transform == target.transform)
+      {
+        fireTimer = weapon.fireDelay;
 
-    void Update()
-    {
-        Vector2 direction = (target.position - transform.position).normalized;
         float degrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        weapon.transform.rotation = Quaternion.Euler(0.0f, 0.0f, degrees);
-        // Combat
-        if (fireTimer > 0.0f)
+        if (BulletPrefab != null)
         {
-            fireTimer -= Time.deltaTime;
-        }
-        if (fireTimer <= 0.0)
-        {
-            Fire(direction);
-        }
-    }
+          var bullet = Instantiate(BulletPrefab, spawnPos, Quaternion.Euler(0.0f, 0.0f, degrees));
 
-    void Fire(Vector2 direction)
+        }
+
+      }
+    }
+  }
+
+  void FixedUpdate()
+  {
+    if (path == null)
     {
-        if (!target) {
-            return;
-        }
-        
-        
-        
-        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
-        Vector3 spawnPos = currentPosition + direction * spawnOffset;
-        
-        RaycastHit2D hitInfo = Physics2D.Raycast(spawnPos, direction);
-        if (hitInfo.collider != null)
-        {
-            Debug.DrawLine(spawnPos, hitInfo.point);
-            if (hitInfo.collider.transform == target.transform)
-            {
-                fireTimer = weapon.fireDelay;
-                
-                float degrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                if (BulletPrefab != null) 
-                {
-                    var bullet = Instantiate(BulletPrefab, spawnPos, Quaternion.Euler(0.0f, 0.0f, degrees));
-
-                }
-                
-            }
-        }
+      return;
     }
-
-    void FixedUpdate()
+    if (currentWaypoint >= path.vectorPath.Count)
     {
-        if (path == null)
-        {
-            return;
-        }
-        if (currentWaypoint >= path.vectorPath.Count)
-        {
-            reachedDestination = true;
-            return;
-        }
-        else
-        {
-            reachedDestination = false;
-        }
-
-        Vector2 waypointTarget = (Vector2)path.vectorPath[currentWaypoint];
-        Vector2 direction = (waypointTarget - rb.position).normalized;
-        Vector2 force = direction * moveSpeed * Time.fixedDeltaTime;
-        rb.AddForce(force);
-
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-
-        if (distance < nextWaypointDist)
-        {
-            currentWaypoint++;
-        }
+      reachedDestination = true;
+      return;
     }
+    else
+    {
+      reachedDestination = false;
+    }
+
+    Vector2 waypointTarget = (Vector2)path.vectorPath[currentWaypoint];
+    Vector2 direction = (waypointTarget - rb.position).normalized;
+    Vector2 force = direction * moveSpeed * Time.fixedDeltaTime;
+    rb.AddForce(force);
+
+    float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+    if (distance < nextWaypointDist)
+    {
+      currentWaypoint++;
+    }
+  }
 }
